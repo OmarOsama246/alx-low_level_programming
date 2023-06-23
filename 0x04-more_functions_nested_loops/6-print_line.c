0893db2a2a2c987ce8a0a653bf5d88e927e4c19a
Omar Osama246 <oo1699081@gmail.com>
2023-06-22 19:12:36 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,7 +8,7 @@
 void print_line(int n)
 {
 	int x;
-	if (n<= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -18,6 +18,6 @@ void print_line(int n)
 		{
 			_putchar(95);
 		}
-		_putchar('\n'):
+		_putchar('\n');
 	}
 }
