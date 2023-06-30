3c86f9eff4bc89a3f9bd6bec41bfcea6a284c21c
Omar Osama246 <oo1699081@gmail.com>
2023-06-29 17:54:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include <string.h>
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -5,7 +6,7 @@ int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
-char *strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
