a2c265dfa08477422cacda77cc93f82ddf3f03d4
Omar Osama246 <oo1699081@gmail.com>
2023-07-05 12:05:26 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _put_ recursion - prints a string, followed by a new line
+ * _put_recursion - prints a string, followed by a new line
  * @s: pointer to the string
  * Return: void
  */
